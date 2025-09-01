90558c157ebd40e2aa7592d3ef52d5e339b411b5
eefe1 <esgiefe@gmail.com>
2025-09-01 13:52:37 +0200

    fix error for nefty

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import netlifyPlugin from '@netlify/vite-plugin-react-router'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), netlifyPlugin()],
   css: {
     preprocessorOptions: {
       scss: {
